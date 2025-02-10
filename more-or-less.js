bd6a8c09ac4f7431746428f80557851fb5f9efbd
Markus Amberla <markus.amberla@gmail.com>
2025-02-10 14:29:30 +0200

    mol01

---
+++ more-or-less.js
@@ -0,0 +1,16 @@
+
+function more(num) {
+    return num++;
+}
+
+function less(num) {
+    return num--;
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}

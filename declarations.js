78cad7a6d7035765ba7275850c4b342224785eae
Markus Amberla <markus.amberla@gmail.com>
2025-02-10 14:15:53 +0200

    declarations04

--- declarations.js
+++ declarations.js
@@ -1,20 +1,16 @@
 const escapeStr = '\`\\\/\"\'';     // actually: '\/"'
-const arr = [4, '2'];
-const obj = {
+const arr = Object.freeze([4, '2']);
+const obj = Object.freeze({
     str: '',
     num: 0,
     bool: false,
     undef: undefined,
-}
-const nested = {
-    arr: [4, undefined, '2'],
-    obj: {
+});
+const nested = Object.freeze({
+    arr: Object.freeze([4, undefined, '2']),
+    obj: Object.freeze({
         str: '',
         num: 0,
         bool: false,
-    },
-}
-
-Object.freeze(arr)
-Object.freeze(obj)
-Object.freeze(nested)
+    }),
+});

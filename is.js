c0a6a892b27f959a2566ea4eaa7148017ad0e1ae
Markus Amberla <markus.amberla@gmail.com>
2025-02-10 16:10:25 +0200

    is02

--- is.js
+++ is.js
@@ -1,12 +1,12 @@
 //const is = {}
 
 is.num = (arg) => typeof arg == 'number'
-is.nan = (arg) => typeof arg == 'NaN'
+is.nan = (arg) => Number.isNan(arg)
 is.str = (arg) => typeof arg == 'string'
 is.bool = (arg) => typeof arg == 'boolean'
 is.undef = (arg) => typeof arg == 'undefined'
 is.def = (arg) => typeof arg != 'undefined'
-is.arr = (arg) => arg.isArray
+is.arr = (arg) => Array.isArray(arg)
 is.obj = (arg) => typeof arg != 'object'
 is.fun = (arg) => typeof arg == 'function'
 is.truthy = (arg) => Boolean(arg)

4004eb967d0b27cc1335745e94d8aabb46cb52ae
Markus Amberla <markus.amberla@gmail.com>
2025-02-10 14:02:27 +0200

    declarations01

---
+++ declarations.js
@@ -0,0 +1,20 @@
+const escapeStr = '\'\\\/\"\'';     // actually: '\/"'"
+const arr = [4, '2'];
+const obj = {
+    str: '',
+    num: 0,
+    bool: false,
+    undef: undefined,
+}
+const nested = {
+    arr: [4, undefined, '2'],
+    obj: {
+        str: '',
+        num: 0,
+        boo: false,
+    },
+}
+
+Object.freeze(arr)
+Object.freeze(obj)
+Object.freeze(nested)

0b91b85ed602950bf5d00c34edc0a31bb8ec95e5
Markus Amberla <markus.amberla@gmail.com>
2025-02-10 16:23:30 +0200

    is05

--- is.js
+++ is.js
@@ -1,8 +1,8 @@
 //const is = {}
 
 is.num = (arg) => typeof arg == 'number'
-//is.nan = (arg) => Number.isNaN(arg)
-is.nan = (arg) => isNaN(arg)
+is.nan = (arg) => Number.isNaN(arg)
+//is.nan = (arg) => isNaN(arg)
 is.str = (arg) => typeof arg == 'string'
 is.bool = (arg) => typeof arg == 'boolean'
 is.undef = (arg) => typeof arg == 'undefined'
@@ -29,3 +29,9 @@ console.log(is.truthy(1))
 console.log(is.truthy(0))
 console.log(is.falsy(0))
 console.log(is.falsy(1)) */
+
+
+/* console.log(is.nan(NaN))
+console.log(is.nan([NaN]))
+console.log(is.nan('sd'))
+console.log(is.nan(12)) */

5e3866ab9d9951cfa1d2c7af42800289ebb96b6f
Markus Amberla <markus.amberla@gmail.com>
2025-02-12 13:30:05 +0200

    get02

--- get.js
+++ get.js
@@ -18,12 +18,33 @@ function get(src, path) {
     let levels = pathToArr(path);
     let thing = src;
     while (levels.length > 0) {
-        thing = thing[levels[0]]; // set value to first available level
-        levels.shift();  // remove first level
+        if (thing[levels[0]] !== undefined) {
+            thing = thing[levels[0]];   // set value to first available level
+            levels.shift();             // remove first level
+        } else {
+            return undefined;
+        }
+
     }
     return thing;
 }
 
 /* const src = { nested: { key: 'peekaboo' } }
 const path = 'nested.key'
-console.log(get(src, path)) // -> 'peekaboo' */
+console.log(get(src, path)) // -> 'peekaboo'
+
+// work with simple key / values
+console.log(get({ key: 'value' }, 'key') === 'value')
+
+// work with nested objects
+console.log(get({ nested: { key: 'value' } }, 'nested.key') === 'value')
+
+// return undefined without error if the value do not exist
+console.log(get({ key: 'value' }, 'nx') === undefined)
+console.log(get({ nested: { key: 'value' } }, 'nested.nx') === undefined)
+console.log(get({ nested: { key: 'value' } }, 'nx.nx') === undefined)
+
+// work with nested arrays too
+let t = "woo";
+console.log(get({ a: [{ b: t }] }, 'a.0.b') === t)
+console.log(get({ a: [{ b: t }] }, 'a.0.b.toString') === t.toString) */

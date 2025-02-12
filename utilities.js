b49b2f766e650762433395ea9359e82623a89518
Markus Amberla <markus.amberla@gmail.com>
2025-02-12 10:11:25 +0200

    slicer01

---
+++ utilities.js
@@ -0,0 +1,15 @@
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    // Sort here if you wish
+    // Clone first if you don't want originals modified
+
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
+
+module.exports = arraysEqual;

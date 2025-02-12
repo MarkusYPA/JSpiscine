7a38143aba048b448b32ba19b866486b67eec3a6
Markus Amberla <markus.amberla@gmail.com>
2025-02-12 10:57:15 +0200

    cutcorners02

--- cut-corners.js
+++ cut-corners.js
@@ -28,13 +28,18 @@ function trunc(num) {
     return divide(num, 1);
 }
 
-/* const nums1 = [3.7, -3.7, 3.1, -3.1]
+/* console.log(divide(0,1))
+console.log(divide(-3,1))
+console.log(divide(3,1))
+console.log(divide(-3.53,1))
+console.log(divide(3.23,1))
+
+const nums1 = [3.7, -3.7, 3.1, -3.1]
 console.log(nums1)
 console.log(nums1.map(round)) // [ 4, -4, 3, -3 ]
 console.log(nums1.map(ceil)) // [ 4, -3, 4, -3 ]
 console.log(nums1.map(floor)) // [ 3, -4, 3, -4 ]
 console.log(nums1.map(trunc)) // [ 3, -3, 3, -3 ]
-
 
 const nums = [Math.PI, -Math.PI, Math.E, -Math.E, 0];
 console.log(arraysEqual(nums.map(round), [3, -3, 3, -3, 0]));
@@ -42,26 +47,11 @@ console.log(arraysEqual(nums.map(ceil), [4, -3, 3, -2, 0]));
 console.log(arraysEqual(nums.map(floor), [3, -4, 2, -3, 0]));
 console.log(arraysEqual(nums.map(trunc), [3, -3, 2, -2, 0])); */
 
-
-function multiply(a, b) {
-    let neg = false;
-    if (b < 0) {
-        neg = true;
-        b = -b;
-    }
-
-    let result = 0;
-    for (let i = 0; i < b; i++) {
-        result += a;
-    }
-
-    if (neg) {
-        return -result;
-    }
-    return result;
-}
-
 function divide(a, b) {
+    if (b == 0) {
+        return 'Error: division by 0';
+    }
+
     let neg = false;
     if (b < 0) {
         neg = !neg;
@@ -84,8 +74,4 @@ function divide(a, b) {
         return -i;
     }
     return i;
-}
-
-function modulo(a, b) {
-    return a - multiply(b, divide(a, b));
 }

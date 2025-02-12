6a419cd07376c64c277e537591288cf7ef792dcd
Markus Amberla <markus.amberla@gmail.com>
2025-02-12 12:13:56 +0200

    reverser04

--- reverser.js
+++ reverser.js
@@ -17,7 +17,7 @@ function reverse(arr) {
     }
 
     if (typeof arr === 'string') {
-        result = '';
+        let result = '';
         for (let i = arr.length-1; i>=0; i--) {
             result += arr[i]
         }

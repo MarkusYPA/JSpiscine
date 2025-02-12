63b8ac257298faade662c0b603edd847d1db015e
Markus Amberla <markus.amberla@gmail.com>
2025-02-12 18:03:45 +0200

    sums01

---
+++ sums.js
@@ -0,0 +1,32 @@
+//'use strict';
+
+function size(arr) {
+    let sum = 0;
+    for (let e of arr) {
+        sum += e;
+    }
+    return sum;
+}
+
+function partitions(target, min = 1, curPart = [], parts = []) {
+    let sz = size(curPart);
+    if (sz === target) {
+        parts.push(curPart);
+        return;
+    }
+    if (sz < target) {
+        for (let i = min; i < target; i++) {
+            let newPart = [...curPart, i];  // New current path, so same doesn't get modified
+            partitions(target, i, newPart, parts);
+        }
+    }
+}
+
+function sums(num) {
+    let parts = [];
+    partitions(num, 1, [], parts);
+    return parts;
+}
+
+//console.log(sums(4))
+//console.log(sums(7))

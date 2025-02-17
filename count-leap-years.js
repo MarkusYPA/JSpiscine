bb0686e5f2a414f6a7d66bc05086fafa52c23944
Markus grit:lab <markus.amberla@gmail.com>
2025-02-17 14:25:26 +0200

    leaps02

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,10 +1,14 @@
 function countLeapYears(date) {
     const yr = date.getFullYear();
     let leaps = 0;
-    for (let i = 4; i <= yr; i+=4) {
-        if (yr % 100 !== 0 || yr % 400 === 0) {
+    for (let i = 4; i < yr; i+=4) {
+        if (i % 400 == 0 || i % 100 != 0) {
+            if (i % 100 == 0) {
+                console.log(i);
+            }
             leaps++;
         }
+        
     }
     return leaps;
 }

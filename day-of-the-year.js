1bc0989d58897dbf8e8c04d71925b5210f6eae2d
Markus grit:lab <markus.amberla@gmail.com>
2025-02-17 14:29:58 +0200

    dayoftheyear02

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -5,5 +5,12 @@ function dayOfTheYear(date) {
     date0.setUTCFullYear(yr, 0, 1); // Year, January 1st
 
     const diffInMs = date.getTime() - date0.getTime();
-    return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+    return Math.floor(diffInMs / (1000 * 60 * 60 * 24)) + 1;
 }
+
+/* console.log(dayOfTheYear(new Date('0001-01-01')), 1)
+console.log(dayOfTheYear(new Date('1664-08-09')), 222)
+console.log(dayOfTheYear(new Date('1600-12-31')), 366)
+console.log(dayOfTheYear(new Date('2020-06-22')), 174)
+console.log(dayOfTheYear(new Date('2048-12-08')), 343)
+console.log(dayOfTheYear(new Date('2048-11-08')), 313) */

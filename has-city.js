7607ce15860c0be7546ee8a2744524f9605647d3
Markus grit:lab <markus.amberla@gmail.com>
2025-02-18 14:10:01 +0200

    hascity02

--- has-city.js
+++ has-city.js
@@ -2,9 +2,9 @@
 function hasCity(country, cities) {
     return ((city) => {
         if (cities.includes(city)) {
-            return city + ' is a city form ' + country; 
+            return city + ' is a city from ' + country; 
         } else {
-            return city + ' is not a city form ' + country; 
+            return city + ' is not a city from ' + country; 
         }
     });
 }

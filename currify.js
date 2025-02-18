3c4e5da79e98aecc5efe98d8bd257dd32f2b21f4
Markus grit:lab <markus.amberla@gmail.com>
2025-02-18 16:09:58 +0200

    curried02

--- currify.js
+++ currify.js
@@ -1,17 +1,29 @@
 'use strict'
 
-function currify(f) {
-    const nargs = f.length;
-    const vargs = [];
+/* function currify(f) {
+    const nargs = f.length;  // Number of parameters f expects
+    const vargs = [];        // Arguments passed in multiple calls, short for "variable arguments"
 
-    const curried = (...args) => vargs.push(...args) >= nargs
-        ? f(...vargs.slice(0, nargs))
-        : curried;
+    const curried = (...args) => vargs.push(...args) >= nargs   // push returns length of array 
+        ? f(...vargs.slice(0, nargs))   // with enough arguments, f(...vargs) is called.
+        : curried;                      // too few arguments, call again 
 
     return curried;
-};
+}; */
 
+function currify(f) {
+    const args = [];
 
+    function curried(...newArgs) {
+        args.push(...newArgs);          // Add new arguments
+        if (args.length >= f.length) {
+            return f(...args);          // Call the original function when enough arguments
+        }
+        return curried; // too few arguments, call curried again (without parentheses)
+    }
+
+    return curried;
+}
 
 /* const mult2 = (el1, el2) => el1 * el2
 const add3 = (el1, el2, el3) => el1 + el2 + el3
@@ -30,4 +42,11 @@ console.log(currify(add3)(4)(7)(10) == 21)
 console.log(currify(sub4)(4)(7)(10)(30) == -43)
 console.log(currify(sub4)(5)(17)(-10)(3) == -5)
 console.log(currify(sub4)(3)(72)(-211)(99) == 43)
-console.log(currify(sub4)(5)(7)(10)(26) == -38) */
+console.log(currify(sub4)(5)(7)(10)(26) == -38)
+
+function add(a, b, c) {
+    return a + b + c;
+}
+
+const curriedAdd = currify(add);
+console.log(curriedAdd(1)(2)(3)); // 6 */
